a1a3956d6e95d4bdedba953a64371b730608a3cd
ChocolateAceCream <nuodi@hotmail.com>
2019-12-27 17:52:28 +0800

    next to update router tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import {SignupComponent} from './auth/signup/signup.component';
 import {LoginComponent} from './auth/login/login.component';
 import {NbConfigComponent} from './nbConfig/nbConfig.component';
 import { AuthGuard } from './auth/auth.guard';
+import { AllNbConfigComponent } from './nbConfig/all-nbConfig/all-nbConfig.component';
+import { NewNbConfigComponent } from './nbConfig/new-nbConfig/new-nbConfig.component';
 
 const routes: Routes = [
     {path: '', component: WelcomeComponent},
     {path: 'signup', component: SignupComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'nbConfig', component: NbConfigComponent, canActivate: [AuthGuard]}
+    {path: 'nbConfig', component: NbConfigComponent, canActivate: [AuthGuard]},
+    {path: 'allConfig', component: AllNbConfigComponent, canActivate: [AuthGuard]},
+    {path: 'newConfig', component: NewNbConfigComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
